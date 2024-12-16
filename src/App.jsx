5e9e560a2cabe2447a14a2a2bd3b18e89dc16ae9
harshita-dundale <harshuprajapati14@gmail.com>
2024-12-16 10:47:13 +0530

    create file structure

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import 'font-awesome/css/font-awesome.min.css';
+
+import LandingPage from './Pages/LandingPage';
+
+
+function App() {
+  return (
+    <>
+    <LandingPage/>
+      </>
+        )
+}
+
+export default App;
