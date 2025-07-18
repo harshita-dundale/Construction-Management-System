196eae97b91ab3883212258fd2d364f6c0dc34c4
shivani-malviya123 <shvanim.bca2023@ssism.org>
2025-07-18 17:56:48 +0530

    add profile and cloudnary use

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,3 +1,19 @@
+// import mongoose from "mongoose";
+
+// const userSchema = new mongoose.Schema(
+//   {
+//     auth0Id: { type: String, required: true, unique: true }, // Unique ID from Auth0
+//     name: { type: String, required: true },
+//     email: { type: String, required: true, unique: true, lowercase: true},
+//     // password: { type: String, required: true },
+//     role: { type: String, enum: ["builder", "worker"], default: " " }, 
+//   },
+//   { timestamps: true }
+// );
+
+// const User = mongoose.model("User", userSchema);
+// export default User;
+
 import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema(
@@ -5,8 +21,8 @@ const userSchema = new mongoose.Schema(
     auth0Id: { type: String, required: true, unique: true }, // Unique ID from Auth0
     name: { type: String, required: true },
     email: { type: String, required: true, unique: true, lowercase: true},
-    // password: { type: String, required: true },
-    role: { type: String, enum: ["builder", "worker"], default: " " }, 
+    role: { type: String, enum: ["builder", "worker"], default: "" },
+    profileImage: { type: String, default: null }, // 🔹 Profile image URL from Cloudinary
   },
   { timestamps: true }
 );
